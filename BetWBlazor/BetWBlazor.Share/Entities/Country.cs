8d1635546d66ccb855fb6fb8825bfce575c7eb8e
Hernán Bs. <bts.hernan@gmail.com>
2024-09-03 08:20:22 -0300

    Add Coutries entity

---
+++ BetWBlazor/BetWBlazor.Share/Entities/Country.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BetWBlazor.Share.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [MaxLength(100)]
+    [Required]
+    public string Name { get; set; } = null!;
+}
